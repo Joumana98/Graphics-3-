d6219c5cd973a321b6356e72c423db02b65bf280
Joumana98 <62801761+Joumana98@users.noreply.github.com>
2023-12-21 12:45:33 +0200

    Update Ex1.cpp

--- Ex1.cpp
+++ Ex1.cpp
@@ -1,3 +1,4 @@
+#include "std_lib_facilities.h"
 #include "Simple_window.h"
 #include "Graph.h"
 
@@ -27,18 +28,31 @@ private:
     Marks eyes;
 };
 
+class Smiley_hat: public Smiley
+{
+
+public:
+    Smiley(Pint c, int r);
+    void draw_lines const();
+
+private:
+    Open_polyline hat;
+};
+
 //-------------------------------------------------------------------------------------------------------------------------------------
 
 int main()
 {
-    using namespace Graph_lib;
+ using namespace Graph_lib;
 
     Simple_window win{Point{20,100},800,600,"My canvas"};
     Smiley face1{Point{200,200},100};
     Frowny face2 {Point{500,100},100};
+    Smiley_hat face3 {Point{200,400},100};
 
     win.attach(face1);
     win.attach(face2);
+    win.attach(face3);
     win.wait_for_button();
 
     return 0;
@@ -80,4 +94,24 @@ void Frowny::draw_lines() const
      eyes.draw_lines();
      mouth.draw_lines();
 
+}
+
+//---------------------------------------------------------------------------------------------------------------------------------------
+
+ Smiley_hat::Smiley_hat(Point c, int r):Smiley{c,r}
+ {
+    double d=r*sqrt(2)/2;
+    add(Point{center().x+d,center().y-d});
+    add(Point{center().x-d,center().y-d});
+
+    hat{point(3),Point{center().x,center().y-2*r},point(4)};
+
+
+ }
+
+
+void Smiley_hat::draw_lines const()
+{
+        Smiley::draw_lines();
+        hat.draw_lines();
 }
